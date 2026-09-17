0a24c68e59650884476602c3a8a917358b280cae
rajsingh10 <rajking.singh10@gmail.com>
2026-09-17 08:10:43 +0530

    feat: register Notification and Setting models and associations

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,8 @@ const sequelize = require('../config/db');
 const User = require('./User');
 const Lead = require('./Lead');
 const Listing = require('./Listing');
+const Notification = require('./Notification');
+const Setting = require('./Setting');
 
 // An agent (User) can have many listings and many leads assigned to them.
 User.hasMany(Listing, { foreignKey: 'agentId', as: 'listings' });
@@ -14,9 +16,16 @@ Lead.belongsTo(User, { foreignKey: 'agentId', as: 'agent' });
 Listing.hasMany(Lead, { foreignKey: 'listingId', as: 'interestedLeads' });
 Lead.belongsTo(Listing, { foreignKey: 'listingId', as: 'listing' });
 
+// Notifications belong to the user who receives them.
+User.hasMany(Notification, { foreignKey: 'userId', as: 'notifications' });
+Notification.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
 module.exports = {
   sequelize,
   User,
   Lead,
   Listing,
+  Notification,
+  Setting,
 };
+
